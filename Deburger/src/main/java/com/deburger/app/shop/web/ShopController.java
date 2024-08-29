a712930702683c56b33572b8169c41ab9efd1a0f
yyoouujin <yujinjann@gmail.com>
2024-08-29 15:50:15 +0900

    레이아웃

---
+++ Deburger/src/main/java/com/deburger/app/shop/web/ShopController.java
@@ -0,0 +1,14 @@
+package com.deburger.app.shop.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+	@GetMapping("shop")
+	public String shopMain() {
+		return "shop/main";
+	}
+	
+}
