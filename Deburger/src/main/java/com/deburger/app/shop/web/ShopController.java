73f12e298c353e4bef3939e54357ae5ae04a33e8
= <=>
2024-08-30 10:30:09 +0900

    2024.08.30 KDH

--- Deburger/src/main/java/com/deburger/app/shop/web/ShopController.java
+++ Deburger/src/main/java/com/deburger/app/shop/web/ShopController.java
@@ -6,9 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ShopController {
 
+	//메인페이지
 	@GetMapping("shop")
 	public String shopMain() {
 		return "shop/main";
 	}
+	
+	//가맹점 재고 관리
+	@GetMapping("stocklist")
+	public String shopstocklist() {
+		return "shop/stocklist";
+	}
+	
+	//가맹점 재고 상세관리
+	@GetMapping("stocklistinfo")
+	public String shopstocklistinfo() {
+		return "shop/stocklistinfo";
+	}
+	
+	//가맹점 수동 발주
+	@GetMapping("shopOrder")
+	public String shopOrder() {
+		return "shop/shopOrder";
+	}
+	
+	//가맹점 자동 발주
+	@GetMapping("shopOrderAuto")
+	public String shopOrderAuto() {
+		return "shop/shopOrderAuto";
+	}
+	
 	
 }
