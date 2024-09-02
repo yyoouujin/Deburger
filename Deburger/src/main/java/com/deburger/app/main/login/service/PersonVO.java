47dd9c505e0192a28cfbe2bfdab1954fb33efa01
admin <admin@YD01-11>
2024-09-02 19:03:12 +0900

    Merge branch 'main' into chc

---
+++ Deburger/src/main/java/com/deburger/app/main/login/service/PersonVO.java
@@ -0,0 +1,15 @@
+package com.deburger.app.main.login.service;
+
+import lombok.Data;
+
+@Data
+public class PersonVO {
+	
+	private String personId;
+	private String name;
+	private String email;
+	private String phone;	
+	private String area;
+	private String department;
+	private String position;
+}
