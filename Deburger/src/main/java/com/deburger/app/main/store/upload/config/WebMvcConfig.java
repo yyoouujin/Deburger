7e234ff25c0588b68f79a5f72f8abf01e61efe11
admin <admin@YD01-11>
2024-09-02 12:17:45 +0900

    dddd

---
+++ Deburger/src/main/java/com/deburger/app/main/store/upload/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.deburger.app.main.store.upload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Value("${file.upload.path}")
+	private String uploadPath;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**") // URL static 폴더안에있는 이름과 달라야함
+		.addResourceLocations("file:///" + uploadPath, "");
+	}
+	
+
+}
