193713be02a09e462ee5f2b422594a5cbe045d86
admin <admin@YD01-11>
2024-09-02 18:25:44 +0900

    ㅇㅇㅇ

--- Deburger/src/main/java/com/deburger/app/main/login/mapper/UserMapper.java
+++ Deburger/src/main/java/com/deburger/app/main/login/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.deburger.app.main.login.mapper;
 
+import com.deburger.app.main.login.service.PersonVO;
 import com.deburger.app.main.login.service.UserVO;
 
 public interface UserMapper {
@@ -9,4 +10,7 @@ public interface UserMapper {
 	
 	// 사용자 등록
 	public int insertUserInfo(UserVO userVO);
+	
+	// 담당자 등록
+	public int insertPersonInfo(PersonVO personVO);
 }
