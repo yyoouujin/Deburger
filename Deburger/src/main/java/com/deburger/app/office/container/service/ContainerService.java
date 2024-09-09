640920f06ec1893bee2d2a2ebc177e7fe8722f3a
=김대운 <=scowl92@naver.com>
2024-09-09 17:56:25 +0900

    0909 김대운

--- Deburger/src/main/java/com/deburger/app/office/container/service/ContainerService.java
+++ Deburger/src/main/java/com/deburger/app/office/container/service/ContainerService.java
@@ -1,6 +1,7 @@
 package com.deburger.app.office.container.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ContainerService {
 
@@ -28,4 +29,12 @@ public interface ContainerService {
 	// 물류 창고 폐기 처리
 	public int disposeItem(ContainerVO containerVO);
 
+	// 물류 창고 출고 조회
+	public List<ContainerVO> containerOutAllList();
+
+	// 물류 창고 출고 상세 조회
+	public List<ContainerVO> containerOutInfo(ContainerVO containerVO);
+
+	// 모달
+	public Map<String, Object> containerOutModalInfo(List<ContainerVO> liSt);
 }
