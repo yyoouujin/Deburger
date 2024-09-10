db8d76ef43d18515887ceb00d0add73ef5ec6d69
= <=>
2024-09-10 17:58:29 +0900

    Merge branch 'main' into KDH

--- Deburger/src/main/java/com/deburger/app/shop/storein/mapper/StoreInMapper.java
+++ Deburger/src/main/java/com/deburger/app/shop/storein/mapper/StoreInMapper.java
@@ -8,7 +8,7 @@ import com.deburger.app.shop.storein.service.StoreInVO;
 public interface StoreInMapper {
 
 	//입고 승인 전체 조회
-	public List<StoreInVO> StoreInList();
+	public List<StoreInVO> StoreInList(StoreInVO storeInVO);
 	
 	//입고 재고 상세 조회
 	public List<StoreInVO> StoreIninfoList(StoreInVO storeInVO);
