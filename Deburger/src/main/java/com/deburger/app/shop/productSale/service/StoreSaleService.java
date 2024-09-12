c501a2e7cf0a38dea9ac477bd4a9b0af82fbf97b
admin <admin@YD01-11>
2024-09-12 19:06:53 +0900

    가맹점 마감 완료

--- Deburger/src/main/java/com/deburger/app/shop/productSale/service/StoreSaleService.java
+++ Deburger/src/main/java/com/deburger/app/shop/productSale/service/StoreSaleService.java
@@ -3,6 +3,14 @@ package com.deburger.app.shop.productSale.service;
 import java.util.List;
 
 public interface StoreSaleService {
+	// 가맹점에서 판매하는 재품 리스트
 	public List<StoreSaleVO> selectStoreSaleList();
+	// 가맹점의 판매등록
 	public int insertStoreSaleList(StoreSaleVO saleVO);
+	// 가맹점 판매등록에따른 제고 변경 시퀀스
+	public void insertStoreSaleProcedure(StoreSaleVO saleVO);
+	// 가맹점 판매등록시 일시통일
+	public StoreSaleVO selectStoreSaleDate();
+	// 가맹점 등록시 매출통계 등록
+	public int insertStoreSaleStatistics(StoreSaleVO saleVO);
 }
