157b3498d3d5d90e25e198e47f7e4cc6c106dc8e
admin <admin@YD01-11>
2024-09-25 09:41:52 +0900

    test

--- Deburger/src/main/java/com/deburger/app/main/store/service/StoreSalesVO.java
+++ Deburger/src/main/java/com/deburger/app/main/store/service/StoreSalesVO.java
@@ -1,10 +1,5 @@
 package com.deburger.app.main.store.service;
 
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
